e1e16c9a52a3f565ac63d3ae746017ec846e71d1
chinazablossom <chinazablossom7@gmail.com>
2025-01-17 16:16:58 +0100

    Stage 1

---
+++ Basic Calculator/task/src/main/java/org/hyperskill/calculator/MainActivity.kt
@@ -0,0 +1,21 @@
+package org.hyperskill.calculator
+
+import android.os.Bundle
+import android.text.InputType
+import androidx.appcompat.app.AppCompatActivity
+import org.hyperskill.calculator.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
+        binding.apply {
+            displayEditText.inputType = InputType.TYPE_NULL
+        }
+
+
+    }
+}
