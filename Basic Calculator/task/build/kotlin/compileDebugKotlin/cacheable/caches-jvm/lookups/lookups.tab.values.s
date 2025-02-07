abfee9099bc4cd42ce4434bd1776f3a823d4b2e4
chinazablossom <chinazablossom7@gmail.com>
2025-02-07 19:50:39 +0100

    Stage 2

---
+++ Basic Calculator/task/build/kotlin/compileDebugKotlin/cacheable/caches-jvm/lookups/lookups.tab.values.s
@@ -0,0 +1,4 @@
+ÑˆÚzÚ{ö{ætÖjòfÖdö^Õ_ÄaútÉkçaòSØLñKÎOøIìAë;÷<À=Æ6×)Øí5ÚýEèÎIÀøGÕÿKÑÖEÆÚGÆòGíüIû	û[ÿ	ÁOÄèE×òEÎÅDÅ÷AÖðCÚÎEñÚEðÈEËËDèïCáðCÊÞCôÎ6Àí(Æ7ÈËðFþ
+êòWÕÎØ[ø
+ÐÕ]ñ
+ÇÔ\ÞÉÏ[äïÄ\ùòöaÅøÆcÐÉúbóÁÞcÝÒÑcÜðùbæþôaãîÄPàöã;Üúë3íþû#ìAÇÑãUöÇ
--- Basic Calculator/task/src/main/java/org/hyperskill/calculator/MainActivity.kt
+++ Basic Calculator/task/src/main/java/org/hyperskill/calculator/MainActivity.kt
@@ -2,20 +2,78 @@ package org.hyperskill.calculator
 
 import android.os.Bundle
 import android.text.InputType
+import android.util.Log
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import org.hyperskill.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val txt = StringBuilder()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        
+
         binding.apply {
             displayEditText.inputType = InputType.TYPE_NULL
+            clearButton.setOnClickListener {
+                txt.clear()
+                displayEditText.text.clear()
+            }
+
+            val digits = listOf(
+                button0, button1, button2, button3,
+                button4, button5, button6, button7,
+                button8, button9,
+            )
+            for (btn in digits) {
+                btn.addNum()
+            }
+            dotButton.addDec()
+
         }
 
-
     }
+
+
+    fun Button.addNum() {
+        binding.apply {
+            this@addNum.setOnClickListener {
+                if (txt.isNotBlank() && txt[0] == '0' && !txt.contains(".") && this@addNum.text == "0") return@setOnClickListener
+
+                txt.append(this@addNum.text).also {
+                    if (it[0] == '0' && !txt.contains(".") && this@addNum.text != "0") txt.deleteCharAt(
+                        0
+                    )
+                }
+
+                displayEditText.setText(txt)
+
+            }
+
+        }
+    }
+
+    fun Button.addDec() {
+        binding.apply {
+            this@addDec.setOnClickListener {
+                if (!txt.contains(".")) {
+                    if (txt.isEmpty()) {
+                        txt.append("0.")
+                        displayEditText.setText(txt)
+
+                    } else {
+                        txt.append(".")
+                        displayEditText.setText(txt)
+                    }
+                }
+
+            }
+
+        }
+    }
+
+
 }
